c90c97a8cb04d8088e46c316a9d397bf81e9f9f6
daisuu01 <daisuu03080723@gmail.com>
2025-11-21 00:59:27 +0900

    pages

--- pages/1_ユーザー_ホーム.py
+++ pages/1_ユーザー_ホーム.py
@@ -152,11 +152,11 @@ with col1:
         """, unsafe_allow_html=True)
 
         if st.button("▶ 開く", use_container_width=True, key="go_chat_new"):
-            st.switch_page("10_ユーザー_チャット.py")
+            st.switch_page("10_ユーザー_チャット")
 
     else:
         if st.button("💬 チャット", use_container_width=True):
-            st.switch_page("10_ユーザー_チャット.py")
+            st.switch_page("10_ユーザー_チャット")
 
 # -------------------------
 # 📝 英作文添削
