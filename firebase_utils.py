0d94b15a575e71aa870e94ab4d0c0e28cd577049
daisuu01 <daisuu03080723@gmail.com>
2025-11-29 15:54:53 +0900

    2

--- firebase_utils.py
+++ firebase_utils.py
@@ -354,12 +354,8 @@ def upload_file_to_storage(uploaded_file, folder_path: str) -> dict:
         dict: {"url": ダウンロードURL, "filename": ファイル名, "size": ファイルサイズ}
     """
     try:
-        # Firebase Storageのバケットを取得（初期化時のバケット名を使用）
-        if STORAGE_BUCKET_NAME:
-            bucket = storage.bucket(STORAGE_BUCKET_NAME)
-        else:
-            # バケット名が取得できない場合はデフォルトバケットを試す
-            bucket = storage.bucket()
+        # Firebase Storageのバケットを取得（バケット名を明示的に指定）
+        bucket = storage.bucket("educa-app2.firebasestorage.app")
         
         # ファイル名にタイムスタンプとUUIDを追加してユニークにする
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
